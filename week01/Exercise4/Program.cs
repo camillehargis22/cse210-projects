a419db0a16ebc96b6029edadace8bcc041d31874
Camille Hargis <camillehargis@studentbody.byu.edu>
2025-07-07 15:45:32 -0600

    w01 exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        // create list
+        List<int> numbers = new List<int>();
+
+        int input = -1;
+
+        // get numbers from user until 0
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+
+        // compute sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // compute average
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // find largest number
+        int max = 0;
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
