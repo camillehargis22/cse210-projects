460ee648594405a083abaaad3f6909aacc1cf699
Camille Hargis <camillehargis@studentbody.byu.edu>
2025-07-07 14:46:11 -0600

    w01 exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // Get the first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // Get the last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Print extra line
+        Console.WriteLine("");
+
+        // Print name
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
