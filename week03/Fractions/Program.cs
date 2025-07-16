1393d30bffce857943e53b6a45eb2ee5b8ad8d3d
Camille Hargis <camillehargis@studentbody.byu.edu>
2025-07-15 22:49:56 -0600

    w03 fraction for encapsulation practice

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        // test setter and getter
+        Fraction f1 = new Fraction();
+        f1.SetTop(6);
+        f1.SetBottom(7);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        // test constructors
+        Fraction f2 = new Fraction();
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(5);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(3, 4);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        Fraction f5 = new Fraction(1, 3);
+        Console.WriteLine(f5.GetFractionString());
+        Console.WriteLine(f5.GetDecimalValue());
     }
 }
